33adad89b9efce24c8f3eaf709e528de119c708f
Adithya <jhadith88@gmail.com>
2024-02-18 20:31:35 +0530

    lil update

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,52 +1,87 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import LiveCursors from './cursor/LiveCursors';
 import { useMyPresence, useOthers } from '@/liveblocks.config';
+import { CursorMode, CursorState } from '@/types/type';
 
 function Live() {
-  const others = useOthers(); //useOthers -> lists all the other users in the room. also extracts data of the users
+  const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+  const [cursorState, setCursorState] = useState<CursorState>({
+    mode: CursorMode.Hidden,
+  });
 
-  //   const handlePointerMove = useCallback((event: React.PointerEvent) => {
+  const setReaction = useCallback((reaction: string) => {
+    setCursorState({ mode: CursorMode.Reaction, reaction, isPressed: false });
+  }, []);
+
+  // {cursorState.mode === CursorMode.ReactionSelector && (
+  //   <ReactionSelector
+  //     setReaction={(reaction) => {
+  //       setReaction(reaction);
+  //     }}
+  //   />
+  // )}
+
+  // const handlePointerMove = useCallback(
+  //   (event: React.PointerEvent) => {
   //     event.preventDefault();
 
   //     const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
   //     const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-  //     updateMyPresence({ cursor: { x, y } });
-  //   }, []);
+  //     // broadcast the cursor position to other users
+  //     updateMyPresence({
+  //       cursor: {
+  //         x,
+  //         y,
+  //       },
+  //     });
+  //   },
+  //   [updateMyPresence]
+  // );
 
-  const handlePointerMove = useCallback(
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+
+    // if cursor is not in reaction selector mode, update the cursor position
+    if (cursor == null || cursorState.mode !== CursorMode.ReactionSelector) {
+      // get the cursor position in the canvas
+      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+      // broadcast the cursor position to other users
+      updateMyPresence({
+        cursor: {
+          x,
+          y,
+        },
+      });
+    }
+  }, []);
+
+  const handlePointerLeave = useCallback(
     (event: React.PointerEvent) => {
       event.preventDefault();
 
-      const { x, y } = event.currentTarget.getBoundingClientRect();
-      const cursorX = event.clientX - x;
-      const cursorY = event.clientY - y;
-
-      updateMyPresence({ cursor: { x: cursorX, y: cursorY } });
+      updateMyPresence({
+        cursor: null,
+        message: null,
+      });
     },
     [updateMyPresence]
   );
 
-  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-    event.preventDefault();
+  const handlePointerDown = useCallback(
+    (event: React.PointerEvent) => {
+      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-    updateMyPresence({
-      cursor: null,
-      message: null,
-    });
-  }, []);
-
-  const handlePointerDown = useCallback((event: React.PointerEvent) => {
-    // event.preventDefault();
-
-    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
-
-    updateMyPresence({
-      cursor: { x, y },
-    });
-  }, []);
+      updateMyPresence({
+        cursor: { x, y },
+      });
+    },
+    [updateMyPresence]
+  );
 
   return (
     <div
